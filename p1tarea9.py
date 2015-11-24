99255d4c14db61373fae46b7de86556343154787
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-11-23 21:01:03 -0300

    Agrego segundo calculo en la P1

--- p1tarea9.py
+++ p1tarea9.py
@@ -19,26 +19,39 @@ S_r = sum(r)
 S_v = sum(v)
 S_rr = sum(x**2 for x in r)
 S_rv = sum(r[i] * v[i] for i in range(n))
+S_vv = sum(x**2 for x in v)
 
-# CÁLCULO DE a Y b
-H_0 = S_rv / S_rr
-print(H_0)
+# PRIMER CÁLCULO DE H
+H1 = S_rv / S_rr
+print(H1)
+
+# SEGUNDO CÁLCULO DE H
+H2 = S_vv / S_rv
+print(H2)
 
 # INTERVALOS DE CONFIANZA
 np.random.seed(1943)
 Nboot = 1000
-mean_values = np.zeros(Nboot)
+mean_values_1 = np.zeros(Nboot)
+mean_values_2 = np.zeros(Nboot)
+mean_final = np.zeros(Nboot)
 
 for i in range(Nboot):
     s = np.random.randint(low=0, high=n, size=n)
-    num = 0
-    den = 0
+    num1 = 0
+    den1 = 0
+    num2 = 0
+    den2 = 0
     for j in s:
-        num += v[j] * r[j]
-        den += r[j]
-    mean_values[i] = np.mean(num / den)
+        num1 += v[j] * r[j]
+        den1 += r[j]**2
+        num2 += v[j]**2
+        den2 += v[j] * r[j]
+    mean_values_1[i] = np.mean(num1 / den1)
+    mean_values_2[i] = np.mean(num2 / den2)
+    mean_final[i] = (mean_values_1[i] + mean_values_2[i]) / 2
 
-orden = np.sort(mean_values)
+orden = np.sort(mean_final)
 low = orden[int(Nboot * 0.025)]
 high = orden[int(Nboot * 0.975)]
 print "El intervalo de confianza al 95% es: [{}:{}]".format(low, high)
--- p2tarea9.py
+++ p2tarea9.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 '''
-Este script calcula la constante de Hubble a partir de datos
-experimentales obtenidos por Freedman et al. publicados el año 2000.
+Este script calcula la constante de Hubble a partir de
+datos experimentales obtenidos en 1929.
 '''
 
 # DATOS
-datos = np.loadtxt("data/SNIa.dat", usecols=(1, 2))
+datos = np.loadtxt("data/hubble_original.dat")
 r = datos[:, 0]
 v = datos[:, 1]
 n = len(r)  # número de galaxias
@@ -19,37 +19,39 @@ S_r = sum(r)
 S_v = sum(v)
 S_rr = sum(x**2 for x in r)
 S_rv = sum(r[i] * v[i] for i in range(n))
+S_vv = sum(x**2 for x in v)
 
-# CÁLCULO DE a Y b
-denom = n * S_rr - S_r**2
-a = (S_rr * S_v - S_r * S_rv) / denom
-b = (n * S_rv - S_r * S_v) / denom
+# PRIMER CÁLCULO DE H
+H1 = S_rv / S_rr
+print(H1)
 
-# DESVIACIÓN ESTÁNDAR DE a Y b
-sigma = np.sqrt(sum((v[i] - (a + b * r[i]))**2 for i in range(n)) / (n - 2))
-sigma_a = np.sqrt((sigma**2 * S_rr) / denom)
-sigma_b = np.sqrt((sigma**2 * n) / denom)
-
-print(a)
-print(b)
-print(sigma_a)
-print(sigma_b)
+# SEGUNDO CÁLCULO DE H
+H2 = S_vv / S_rv
+print(H2)
 
 # INTERVALOS DE CONFIANZA
 np.random.seed(1943)
-Nboot = 10000
-num = np.zeros(Nboot)
-den = np.zeros(Nboot)
-mean_values = np.zeros(Nboot)
+Nboot = 1000
+mean_values_1 = np.zeros(Nboot)
+mean_values_2 = np.zeros(Nboot)
+mean_final = np.zeros(Nboot)
 
 for i in range(Nboot):
     s = np.random.randint(low=0, high=n, size=n)
+    num1 = 0
+    den1 = 0
+    num2 = 0
+    den2 = 0
     for j in s:
-        num += n * r[j] * v[j] - r[j] * S_v
-        den += n * r[j]**2 - r[j] * S_r
-    mean_values[i] = np.mean(num / den)
+        num1 += v[j] * r[j]
+        den1 += r[j]**2
+        num2 += v[j]**2
+        den2 += v[j] * r[j]
+    mean_values_1[i] = np.mean(num1 / den1)
+    mean_values_2[i] = np.mean(num2 / den2)
+    mean_final[i] = (mean_values_1[i] + mean_values_2[i]) / 2
 
-orden = np.sort(mean_values)
+orden = np.sort(mean_final)
 low = orden[int(Nboot * 0.025)]
 high = orden[int(Nboot * 0.975)]
 print "El intervalo de confianza al 95% es: [{}:{}]".format(low, high)
